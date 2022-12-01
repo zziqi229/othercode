8c897f1ace78d5427aae96d47559ec3688da22ea
zzq229-creator <66601730+zzq229-creator@users.noreply.github.com>
2022-12-01 20:00:41 +0800

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+F="proxy-admin_linux-amd64.tar.gz"
+set -e
+if [ -e /data/data/com.termux/proxy ]; then
+    rm -rf /data/data/com.termux/proxy
+fi
+mkdir /data/data/com.termux/proxy
+cd /data/data/com.termux/proxy
+echo -e "\n>>> downloading ... $F\n"
+
+manual="https://snail.gitee.io/proxy/manual/zh/"
+LAST_VERSION=$(curl --silent "https://mirrors.host900.com/https://api.github.com/repos/snail007/proxy_admin_free/releases/latest" | grep -Po '"tag_name":"\K.*?(?=")')
+wget  -t 1 "https://mirrors.host900.com/https://github.com/snail007/proxy_admin_free/releases/download/${LAST_VERSION}/$F"
+
+echo -e ">>> installing ... \n"
+#install proxy-admin
+tar zxvf $F >/dev/null 2>&1 
+chmod +x proxy-admin
+set +e
+./proxy-admin uninstall >/dev/null 2>&1 
+set -e
+./proxy-admin install
+./proxy-admin start
+rm $F
+set +e
+systemctl status proxyadmin &
+set -e
+sleep 2
+echo  -e "\n>>> install done, thanks for using snail007/proxy-admin\n"
+echo  -e ">>> install path /usr/local/bin/proxy-admin\n"
+echo  -e ">>> configuration path /etc/gpa\n"
+echo  -e ">>> uninstall just exec : proxy-admin uninstall\n"
+echo  -e ">>> please visit : http://YOUR_IP:32080/ username: root, password: 123\n"
+echo  -e ">>> How to using? Please visit : $manual\n"
